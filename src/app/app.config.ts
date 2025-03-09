25c5586f97417fe95c32bfc467fb613cb8581259
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 00:53:39 -0500

    feat: add ResponseInterceptor to HTTP interceptors in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { withInterceptorsFromDi, HTTP_INTERCEPTORS, provideHttpClient, withFetch } from "@angular/common/http";
+import { ResponseInterceptor } from "./interceptors/response.interceptor";
 import { provideRouter, withViewTransitions } from "@angular/router";
 import { ApplicationConfig } from "@angular/core";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions()), provideHttpClient(withInterceptorsFromDi(), withFetch())],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    provideRouter(routes, withViewTransitions()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ResponseInterceptor,
+      multi: true,
+    },
+  ],
 };
