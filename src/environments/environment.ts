f0c17a956c6264c3d2bded18545c628b9dc972be
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 16:58:47 -0500

    feat: add environment configuration for local development

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  storageFront: 'http://localhost:4200/assets',
+  storageBack: 'http://localhost:3000/storage',
+  apiUrl: 'http://localhost:3000/api/',
+  production: false,
+};
