8dafafd60bd5170ada7ad2b934d2de4803557f26
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 01:24:47 -0500

    feat: add title to management route for better user context

--- src/app/components/pages/page.routes.ts
+++ src/app/components/pages/page.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from "@angular/router";
 
 export const PageRoutes: Routes = [
   {
+    title: "Gestor de tareas",
     path: "management",
     canActivate: [AuthGuard],
     loadComponent: () => import("@components/pages/management/management.component").then(c => c.ManagementComponent),
