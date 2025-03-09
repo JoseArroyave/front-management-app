f230ffc9faeea322988a92bf4449a8ca6c7e0617
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 16:33:02 -0500

    Auto-generated commit

--- chunk-H4I2XDLC.js
+++
@@ -1,1 +0,0 @@
-var t={storageFront:"http://37.60.237.211.co/assets",storageBack:"http://37.60.237.211.co/storage",apiUrl:"http://37.60.237.211.co/api/",production:!0};export{t as a};
---
+++ chunk-YVELGN4G.js
@@ -0,0 +1,1 @@
+var t={storageFront:"http://37.60.237.211/assets",storageBack:"http://37.60.237.211/storage",apiUrl:"http://37.60.237.211/api/",production:!0};export{t as a};
