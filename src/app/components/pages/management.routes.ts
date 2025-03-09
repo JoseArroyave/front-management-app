3dca1cc426ca31b8961eab0ee619359c9f38031d
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 00:54:10 -0500

    feat: remove management component and associated routes

--- src/app/components/pages/management.routes.ts
+++
@@ -1,5 +0,0 @@
-// import { AccessGuard } from '@guards/access.guard';
-// import { AuthGuard } from '@guards/auth.guard';
-import { Routes } from "@angular/router";
-
-export const ManagementRoutes: Routes = [{ path: "**", redirectTo: "public/not-found" }];
