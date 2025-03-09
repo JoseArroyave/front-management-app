0d2b812996ff5412c4f73cbd3246da694a87d040
Jose Arroyave <poer0601@hotmail.com>
2025-03-09 16:32:57 -0500

    feat: add production environment configuration and file replacements in angular.json

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  storageFront: "http://37.60.237.211/assets",
+  storageBack: "http://37.60.237.211/storage",
+  apiUrl: "http://37.60.237.211/api/",
+  production: true,
+};
